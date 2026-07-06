d6f3a77becaf9eb48441457ac3a7f9ed04f1da38
Sammy65433 <samiamlistening@gmail.com>
2026-07-06 13:25:28 -0400

    Exercise-Collaborating on an Express-MongoDB App 2

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./data /db";
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+
+
+app.use(express.json()); //register express built in json
+
+app.use(express.urlencoded({ // lets you reaad from data in req.body
+    extended: true
+}));
+
+app.use('/static', express.static("public"));
+
+app.use("movies", )
+
+app.get('/', (req, res) => {
+    res.render('test');
+})
+
+
+app.listen(PORT, () => {
+    
+    console.log(`Server running on port ${PORT}`);
+connectDB()
+});
+
+
+
